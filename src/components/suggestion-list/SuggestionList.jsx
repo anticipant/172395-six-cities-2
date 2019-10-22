5c728bb311f28599002300a1e829972ea74ffc36
anticipant <shakhovich.d@yandex.ru>
2019-10-22 23:58:54 +0300

    [FE] feat: - add props; - snapShot tests for App and SuggestionList components; - add e2e test to App component;

--- src/components/suggestion-list/SuggestionList.jsx
+++ src/components/suggestion-list/SuggestionList.jsx
@@ -72,7 +72,10 @@ const SuggestionList = (props) => {
               <h2 className="visually-hidden">Places</h2>
               <b className="places__found">312 places to stay in Amsterdam</b>
               <form className="places__sorting" action="#" method="get">
-                <span className="places__sorting-caption">Sort by</span>
+                <span
+                  onClick={props.onClickHandler}
+                  className="places__sorting-caption"
+                >Sort by</span>
                 <span className="places__sorting-type" tabIndex="0">
                   Popular
                   <svg className="places__sorting-arrow" width="7" height="4">
@@ -141,6 +144,7 @@ const SuggestionList = (props) => {
 
 SuggestionList.propTypes = {
   cards: PropTypes.array.isRequired,
+  onClickHandler: PropTypes.func.isRequired,
 };
 
 export default SuggestionList;
--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import App from "./components/app/App";
 const init = () => {
   const settings = {
     cards: [`Beautiful & luxurious apartment at great location`, `Wood and stone place`],
+    onClickHandler: () => {},
   };
 
   ReactDOM.render(
