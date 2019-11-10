8e00d4d64e46d2eb0c1bf8a1cd7a99da22c07417
anticipant <shakhovich.d@yandex.ru>
2019-11-10 21:32:26 +0300

    [FE] refactor: создал новый компонент для главной страницы;

---
+++ src/components/main-page/MainPage.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import MainPage from "./MainPage";
+import renderer from 'react-test-renderer';
+import Map from "../map/Map";
+import {leaflet} from "../../mocks/leaflet";
+import {getTestCard} from "../../mocks/get-test-card";
+
+it(`MainPage correctly renders after relaunch`, () => {
+  const offers = [getTestCard(`MainPage`, 999, false)];
+
+  const tree = renderer.create(<MainPage
+    cards={offers}
+    onCardClick={() => {}}
+    name={`Amsterdam`}
+  >
+    <Map name={`MainPage`} offers={offers} leaflet={leaflet}/>
+  </MainPage>).toJSON();
+  expect(tree).toMatchSnapshot();
+});
