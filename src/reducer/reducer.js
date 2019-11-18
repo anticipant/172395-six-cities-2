a8d9ff2ef374c450daea3be8b179ca1e4112781c
anticipant <shakhovich.d@yandex.ru>
2019-11-18 23:17:48 +0300

    [FE] feat: создаю reducer, actions и store;

---
+++ src/reducer/reducer.js
@@ -0,0 +1,28 @@
+import {offers} from "../mocks/offers";
+import {SELECT_CITY} from "../actions/select-city-action";
+import {SET_OFFERS_LIST} from "../actions/set-offers-list-action";
+
+const initialState = {
+  activeCity: `Amsterdam`,
+  offers,
+  offersList: offers.filter((it) => it.city.name === `Amsterdam`),
+};
+
+export const reducer = (state = initialState, action) => {
+
+  switch (action.type) {
+    case SELECT_CITY:
+      return Object.assign({}, state, {
+        activeCity: action.payload,
+      });
+
+    case SET_OFFERS_LIST:
+      console.log(`SET_OFFERS_LIST`, SET_OFFERS_LIST);
+      const filteredOffers = state.offers.filter((it) => it.city.name === action.payload);
+      return Object.assign({}, state, {
+        offersList: filteredOffers,
+      });
+    default:
+      return state;
+  }
+};
